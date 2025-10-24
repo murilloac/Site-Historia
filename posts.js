08f3a031c70f991ec0166e1595389cec655f6a9e
Murillo A Cunha <murilloac92@gmail.com>
2025-10-24 10:39:09 -0300

    terceiro commit do projeto

--- posts.js
+++ posts.js
@@ -6,6 +6,6 @@ const posts = [
     descricao: "Tomando o Novo Testamento como base, percebemos que todo o cristianismo — que se estende até os dias atuais — é fundamentado na fé e em relatos documentais resgatados por historiadores.",
     imagem: "https://i.imgur.com/1Gt3ccE.jpeg",
     tags: ["historia-religiosa"],
-    url: "../front/novoTestamento.html"
+    url: "novoTestamento.html"
   },
 ];
